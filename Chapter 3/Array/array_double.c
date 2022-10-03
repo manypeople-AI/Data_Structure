2bb29462123337a0f52a29c44bacfe670f425c4e
Lee Dain <76834485+manypeople-AI@users.noreply.github.com>
2022-10-03 22:50:54 +0900

    update

--- Chapter 3/Array/array_double.c
+++ Chapter 3/Array/array_double.c
@@ -6,7 +6,7 @@ int main()
     int numArr[10] = { 11, 22, 33, 44, 55, 66, 77, 88, 99, 110 };    // 크기가 10인 int형 배열
 
     for (int i=0;i<sizeof(numArr)/sizeof(int);i++){
-        numArr[i] = 2*numArr[i];
+        numArr[i] = 2*numArr[i]; //numArr[i]*=2
         printf("%d\n",numArr[i]);}
    
 
--- Chapter 3/Pointer/pointer.c
+++ Chapter 3/Pointer/pointer.c
@@ -11,6 +11,8 @@ int main()
     char c1 = 'a';         // 변수에 문자 'a' 저장
     char *s1 = "Hello";    // 포인터에 문자열 "Hello"의 주소 저장
 
+    int *numPtr;
+    printf("%lu\n", sizeof(s1));    // 8: 64비트에서 int 포인터는 8바이트
     printf("%c\n", c1);    // a: %c로 문자 출력
     printf("%s\n", s1);    // Hello: %s로 문자열 출력
 
