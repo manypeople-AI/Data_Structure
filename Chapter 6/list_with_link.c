06563cf02e9e23e8da109f21c200a084843d2343
Lee Dain <76834485+manypeople-AI@users.noreply.github.com>
2022-10-15 20:38:41 +0900

    update

--- Chapter 6/list_with_link.c
+++ Chapter 6/list_with_link.c
@@ -11,10 +11,30 @@ typedef struct ListNode
     struct ListNode *link; // link field : 포인터가 들어감
 }ListNode;
 
-ListNode *head = NULL;
-//노드의 크기만큼 동적메모리를 할당받음
-//이 동적 메모리가 하나의 노드가 된다 동적메모리 주소를 head에 저장
-head = (ListNode *)malloc(sizeof(ListNode));
+// 전역 범위에서는 함수호출을 할 수 없음 반드시 어떤 함수이 내부에서 해야함
+void list_with_link(void){
+    // head pointer
+    ListNode *head = NULL;
+    // head는 첫번째 노드를 가르키는 주소를 가짐
+    //노드의 크기만큼 동적메모리를 할당받음
+    //이 동적 메모리가 하나의 노드가 된다 동적메모리 주소를 head에 저장
+    head = (ListNode *)malloc(sizeof(ListNode));
+    
+    head->data = 10;
+    head->link = NULL;
 
-head->data = 10;
-head->link = NULL;
+    // 두번째 노드의 생성
+    ListNode *p;
+    p = (ListNode *)malloc(sizeof(ListNode));
+    p-> data =20;
+    p-> link =NULL; // 마지막 NODE니까 NULL
+
+    // 노드의 연결
+    head->link = p;
+
+}
+
+
+
+
+
---
+++ Chapter 6/word_link_list.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    char name[100]
+}element;
+
+//노드의 구조 정의(not 생성)
+typedef struct ListNode //구조체이름
+{
+    element data; // Data Field
+    struct ListNode *link; // link field : 포인터가 들어감
+}ListNode;//구조체 별칭
+
+// ListNode *p = NULL; //현재 아무것도 가리키고 있지 않은 상태
+// p = (ListNode *)malloc(sizeof(ListNode)); //
+ListNode* insert_first(ListNode *head, element value){ // head는 헤드포인터, value는 새롭게 추가되는 데이터 
+    ListNode *p = (ListNode *)malloc(sizeof(ListNode)); //새로 추가되는 데이터 이제 p 포인터가 얘를 가리켜야함
+    p->data = value;
+    p->link = head; // p 포인터의 값을 복사
+    //head가 p를 가리키도록 함 head는 그냥 포인터임(값 x)
+    head=p; //헤드포인터 변경
+    return head;
+}
+
+ListNode* insert(ListNode *head, ListNode *pre, element value){
+    ListNode *p = (ListNode *)malloc(sizeof(ListNode));
+    p->data = value;
+    p-> link = pre->link;
+    
+    pre->link = p; //p라는 struct를 가리킴
+        
+    return head;
+
+}
+
+//첫번째 노드 삭제 함수
+ListNode* delete_first(ListNode *head){
+    if (head == NULL) return NULL;
+    ListNode* removed = head;
+    head = removed->link;
+    free(removed);
+    return head;
+}
+
+//중간의 노드 삭제 함수
+
+ListNode* delete(ListNode *head, ListNode *pre){
+    ListNode* removed = pre->link;
+    pre->link = removed->link;
+    free(removed);
+    return head;
+
+}
+//print_list 함수
+
+void print_list(ListNode* head){
+    int len=0;
+    for (ListNode* p=head;p!=NULL;p = p->link){
+        printf("num: %s -> ",p->data);
+    }
+    printf("NULL \n");
+    
+}
+
+int main (void){
+    ListNode *head = NULL;
+
+    for (int i = 0;i<5;i++){
+        head = insert_first(head,i);
+        print_list(head);
+    }
+    for (int i = 0; i<5;i++){
+        head = delete_first(head);
+        print_list(head);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
