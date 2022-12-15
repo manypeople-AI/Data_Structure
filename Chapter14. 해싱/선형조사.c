e69c6f67d759f4b2dc852768cf369d016b00d750
Lee Dain <76834485+manypeople-AI@users.noreply.github.com>
2022-12-16 08:26:59 +0900

    final

---
+++ Chapter14. 해싱/선형조사.c
@@ -0,0 +1,17 @@
+#define KEY_SIZE 10 //탐색키의 최대길이
+#define TABLE_SIZE 13 //해싱 테이블의 크기 = 소수
+#include <stdio.h>
+
+
+typedef struct
+{
+    char key[KEY_SIZE];
+}element;
+
+
+
+int main(void){
+    printf("%d",0%3);
+
+    return 0;
+}
---
+++ Chapter14. 해싱/체이닝.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TABLE_SIZE 7
+
+typedef struct
+{
+    int key;
+}element;
+
+struct list{ // 하나의 버킷을 연결리스트로 구현
+    element item;
+    struct list *link;
+};
+struct list *hash_table[TABLE_SIZE]; 
+
+//제산 함수를 사용한 해싱함수
+int hash_function(int key){
+    return key % TABLE_SIZE;
+}
+
+// 체인법을 이용하여 테이블에 키를 삽입
